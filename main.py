ccdccc2ec4b9222a71dda8620c04bc4e3db937a9
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2025-02-10 17:55:12 +0000

    1

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    player_1 = input("Player 1, Enter in x on where you want to go")
+    player_1 = title(player_1)
+    player_2 = input("Player 2, Enter in o on where you want to go")
+    player_2 = title(player_2)
+
