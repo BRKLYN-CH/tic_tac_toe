56f3b647c0a6baa4cb992b960d4f13d8e4489219
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2025-02-19 17:50:40 +0000

    3

--- main.py
+++ main.py
@@ -1,28 +1,52 @@
+location_map={
+    1: (0,0),
+    2: (0,1),
+    3: (0,2),
+    4: (1,0),
+    5: (1,1),
+    6: (1,2),
+    7: (2,0),
+    8: (2,1),
+    9: (2,2)
+    }
+
+def game_board(board):
+    board =[
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9']
+    ]
+    return board
+
 def player_choice():
-    try:
-        player_1 = input("Pick a square(X or O): ")
-    except ValueError:
-        print("That is not a valid option")
-    player_1 = player_1.title()
-    try:
-        player_2 = input("Pick a square(X or O): ")
-    except ValueError:
-        print("That is not a valid option")
-    player_2 = player_2.title()
-    print(player_1)
-    print(player_2)
-
-    user_input = input("X or O")
-    user.is alpha
-    while loop 
-
+    while True:
+        player_1 = input("Player 1's choice, are you an X or an O: ")
+        player_1 = player_1.title()
+        if player_1.isalpha() is True:
+            if player_1 == "X" or player_1 == "O":
+                break
+            else:
+                continue
+        else:
+            continue
+    while True:
+        player_2 = input("Player 2's choice, are you an X or an O: ")
+        player_2 = player_2.title()
+        if player_2.isalpha() is True:
+            if player_2 == "X" or player_2 == "O":
+                break
+            else:
+                continue
+        else:
+            continue
 
 
 def players_score(scores):
-    scores = {'Player 1': 0, 'Player 2': 0}
+    scores = {'Player 1': 0, 'Player 2': 0, 'Draw': 0}
     scores['Player 1'] += 1
     scores['Player 2'] += 1
     print(scores)
+
 
 """
 def check_winner(board, scores,):
@@ -34,4 +58,4 @@ def check_winner(board, scores,):
         if all column == X:
             return scores['Player 1 or 2'] += 1
 """
-player_choice()
+game_board(board=0)
