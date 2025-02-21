db5b3f56194d2dc4ffb2559ae1593081a19424c1
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2025-02-21 17:55:43 +0000

    5

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-def player_choice():
+game_board = [
+    ["-","-","-",],
+    ["-","-","-"],
+    ["-","-","-"]
+]
+different_board = [
+    ["1","2","3",],
+    ["4","5","6"],
+    ["7","8","9"]
+]
+
+def print_current_board():
+    for row in range(len(game_board)):
+        for column in range(len(game_board)):
+            print(f"{game_board[row][column]:<5}", end = "")
+        print("")
+
+def print_different_board():
+    for row in range(len(different_board)):
+        for column in range(len(different_board)):
+            print(f"{different_board[row][column]:<5}", end = "")
+        print("")
+
+def check_for_winner(game_board):
+    if game_board[0][0] == game_board[0][1] and game_board[0][1] == game_board[0][2]:
+        print("We have a winner!")
+
+def players_score(scores):
+    scores = {'Player 1': 0, 'Player 2': 0}
+    scores['Player 1'] += 1
+    print(scores)
+
+def player_choice(game_board):
+    print_current_board()
     while True:
-        player_1 = input("Player 1's choice, are you an X or an O: ")
-        player_1 = player_1.title()
+        player_1 = input("Player 1, choose X or O: ")
+        player_1 = player_1.upper()
         if player_1.isalpha() is True:
             if player_1 == "X" or player_1 == "O":
                 break
@@ -9,41 +42,61 @@ def player_choice():
                 continue
         else:
             continue
-    while True:
-        player_2 = input("Player 2's choice, are you an X or an O: ")
-        player_2 = player_2.title()
-        if player_2.isalpha() is True:
-            if player_2 == "X" or player_2 == "O":
+    if player_1 == "X":
+        player_2 = "O"
+    else:
+        player_2 = "X"
+    print(f"Player 1 is {player_1} and Player 2 is {player_2}")
+
+    print_different_board()
+
+    while game_board[0][0] == "-" or game_board[0][1] == "-" or game_board[0][2] == "-" or game_board[1][0] == "-" or game_board[1][1] == "-" or game_board[1][2] == "-" or game_board[2][0] == "-" or game_board[2][1] == "-" or game_board[2][2] == "-":
+        
+        while True:
+            player_one_turn = int(input("Player 1, enter the number for the spot do you want to take: "))
+
+            if player_one_turn == 1 or player_one_turn == 2 or player_one_turn == 3:
+                p_1_row = 0
+            elif player_one_turn == 4 or player_one_turn == 5 or player_one_turn == 6:
+                p_1_row = 1
+            else:
+                p_1_row = 2
+            if player_one_turn == 1 or player_one_turn == 4 or player_one_turn == 7:
+                p_1_column = 0
+            elif player_one_turn == 2 or player_one_turn == 5 or player_one_turn == 8:
+                p_1_column = 1
+            else:
+                p_1_column = 2
+
+            if game_board[p_1_row][p_1_column] == "-":
+                game_board[p_1_row][p_1_column] = player_1
                 break
             else:
+                print("That spot is taken, try again:")
                 continue
+
+        print_current_board()
+
+
+        player_two_turn = int(input("Player 2, enter the number for the spot do you want to take: "))
+
+        if player_two_turn == 1 or player_two_turn == 2 or player_two_turn == 3:
+            p_2_row = 0
+        elif player_two_turn == 4 or player_two_turn == 5 or player_two_turn == 6:
+            p_2_row = 1
         else:
-            continue
-    """
-    Ask them where they want to go and implement it. 
-    go to update board and have that check if the square is open
-    """
+            p_2_row = 2
+        if player_two_turn == 1 or player_two_turn == 4 or player_two_turn == 7:
+            p_2_column = 0
+        elif player_two_turn == 2 or player_two_turn == 5 or player_two_turn == 8:
+            p_2_column = 1
+        else:
+            p_2_column = 2
 
+        if game_board[p_2_row][p_2_column] == "-":
+            game_board[p_2_row][p_2_column] = player_2
 
-def players_score(scores):
-    scores = {'Player 1': 0, 'Player 2': 0, 'Draw': 0}
-    scores['Player 1'] += 1
-    scores['Player 2'] += 1
-    print(scores)
-
-
-"""
-def check_winner(board, scores):
-    for row in board:
-        if all row == X:
-            return scores['Player 1 or 2'] += 1
-
-    for column in board:
-        if all column == X:
-            return scores['Player 1 or 2'] += 1
-
-    for digainl in board == x:
-"""
+        print_current_board()
 
 def main():
     """ 
@@ -54,3 +107,7 @@ def main():
     check it won(break if won)
     repeat
     """
+    while True:
+        player_choice(game_board)
+        if 
+
